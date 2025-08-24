38d610582c1c0d473cd98951d77217420c0e65aa
yixuan325 <angle930325@gmail.com>
2025-08-24 14:27:23 +0000

    fix: 修復App.tsx上onLogout的錯誤

--- src/components/SidebarLayout.tsx
+++ src/components/SidebarLayout.tsx
@@ -1,23 +1,25 @@
-import { type FC, type ReactNode } from "react";
+import { type FC } from "react";
 import { Outlet } from "react-router-dom";
 import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
-import { AppSidebar } from "./AppSidebar"; // 你上面那個 Sidebar
+import { AppSidebar } from "./AppSidebar";
+import { AppHeader } from "./AppHeader";
 
 interface SidebarLayoutProps {
-  children?: ReactNode;
+  onLogout?: () => void;
 }
 
-export const SidebarLayout: FC<SidebarLayoutProps> = () => {
+export const SidebarLayout: FC<SidebarLayoutProps> = ({ onLogout }) => {
   return (
     <SidebarProvider>
       <div className="flex min-h-screen w-full">
-        {/* Sidebar */}
+        {/* 左側 Sidebar */}
         <AppSidebar />
 
-        {/* 主內容區 */}
+        {/* 右側主畫面 */}
         <SidebarInset className="flex flex-col flex-1">
+          <AppHeader onLogout={onLogout} />
           <main className="flex-1 overflow-auto p-4">
-            <Outlet />
+            <Outlet /> {/* 子頁面會渲染在這 */}
           </main>
         </SidebarInset>
       </div>
